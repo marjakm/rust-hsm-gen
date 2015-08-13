8733bda181c4485b62bb722d7414d84d728b6245
Mattis Marjak <mattis.marjak@gmail.com>
2015-08-14 00:48:41 +0300

    test ast modification

--- src/lib.rs
+++ src/lib.rs
@@ -15,13 +15,14 @@ mod test {
     use syntax::parse;
     use syntax::ext;
     use syntax::print;
+    use syntax::codemap::{ExpnId, ExpnInfo, CompilerExpansion, NameAndSpan};
 
     use rustc::session::{build_session,Session};
     use rustc::session::config::{build_session_options,build_configuration, Input};
     use rustc_driver::{handle_options,diagnostics_registry};
     use rustc_driver::driver::{phase_1_parse_input, source_name};
 
-    fn pprint(sess: Session, krate: &ast::Crate, src_name: String) {
+    fn pprint(sess: &Session, krate: &ast::Crate, src_name: String) {
         let src = sess.codemap().get_filemap(&src_name[..])
                             .src
                             .as_ref()
@@ -62,24 +63,48 @@ mod test {
         let sess = build_session(sopts, None, descriptions);
         let cfg = build_configuration(&sess);
         let input = Input::Str(crate_src);
-        let krate = phase_1_parse_input(&sess, cfg, &input);
+        let src_name = source_name(&input);
+
+        let features = sess.features.borrow();
+        let ecfg = ext::expand::ExpansionConfig {
+            crate_name: src_name.clone(),
+            features: Some(&features),
+            recursion_limit: sess.recursion_limit.get(),
+            trace_mac: sess.opts.debugging_opts.trace_macros,
+        };
+
+
+        let mut krate = phase_1_parse_input(&sess, cfg.clone(), &input);
+        // println!("{:#?}", krate);
+        {
+            let mut cx = ExtCtxt::new(&sess.parse_sess, cfg, ecfg);
+            cx.backtrace = ExpnId::from_u32(0);
+            cx.codemap().record_expansion(
+                ExpnInfo {
+                    call_site: krate.span,
+                    callee: NameAndSpan {
+                        name: "mattis".to_string(),
+                        format: CompilerExpansion,
+                        allow_internal_unstable: true,
+                        span: None,
+                    },
+                }
+            );
+
+            let x = quote_item!(&cx,
+                fn foo() -> u32 {
+                    let sum = 2 + 2;
+                    sum
+                }
+            ).unwrap();
+            krate.module.items.push(x);
+        }
         println!("{:#?}", krate);
 
-        let src_name = source_name(&input);
-        pprint(sess, &krate, src_name);
+        pprint(&sess, &krate, src_name);
 
 
         panic!("Lõpp");
-        // let exc = ext::expand::ExpansionConfig::default(cratenam);
-        // let cx = ExtCtxt::new(
-        //     &sess.parse_sess,
-        //     cfg,
-        //     exc,
-        // );
-        // //
-        // let x = quote_expr!(&cx, 1 + 2);
-        // stringify!(x);
-        // // println!("{:?}", );
     }
 
 }
