cfd836d0cf3fcd11bd314451263cfbc3fad95b30
Mattis Marjak <mattis.marjak@gmail.com>
2015-08-17 22:52:40 +0300

    working XmiReader

--- src/xmi.rs
+++ src/xmi.rs
@@ -1,3 +1,4 @@
+use std::mem;
 use std::io::prelude::*;
 use std::fs::File;
 use std::collections::HashMap;
@@ -8,25 +9,56 @@ use sxd_document::writer::format_document;
 use sxd_document::parser::Parser;
 use sxd_xpath::{Value,Functions,Variables,Namespaces,Factory,EvaluationContext,Expression};
 use sxd_xpath::function::register_core_functions;
+use sxd_xpath::nodeset::Node;
 
-pub struct XmiReader<'a> {
-    package     : Package,
+
+struct InnerXmiReader <'d> {
+    package : Package,
+    doc     : Document<'d>,
+}
+impl<'d> InnerXmiReader<'d> {
+    pub fn new(pkg: Package) -> Self {
+        InnerXmiReader {
+            package : pkg,
+            doc     : unsafe {mem::uninitialized()},
+        }
+    }
+    pub fn init(&self) {
+        unsafe {
+            let mut dest: *mut Document = mem::transmute(&self.doc);
+            *dest = self.package.as_document();
+        }
+    }
+    pub fn doc(&self) -> &Document<'d> {
+        &self.doc
+    }
+}
+
+
+pub struct XmiReader<'a, 'd> {
+    inner       : Box<InnerXmiReader<'d>>,
     functions   : Functions,
     variables   : Variables<'a>,
     namespaces  : Namespaces,
     factory     : Factory,
 }
-impl<'a> XmiReader<'a> {
-    pub fn new() -> Self {
+impl<'a:'d, 'd> XmiReader<'a, 'd> {
+    fn _new(pkg: Package) -> Self {
         let mut fns = HashMap::new();
         register_core_functions(&mut fns);
-        XmiReader {
-            package     : Package::new(),
+        let xr = XmiReader {
+            inner       : Box::new(InnerXmiReader::new(pkg)),
             functions   : fns,
             variables   : HashMap::new(),
             namespaces  : HashMap::new(),
             factory     : Factory::new(),
-        }
+        };
+        xr.inner.init();
+        xr
+    }
+
+    pub fn new() -> Self {
+        XmiReader::_new(Package::new())
     }
 
     pub fn from_file(file: &str) -> Self {
@@ -36,15 +68,17 @@ impl<'a> XmiReader<'a> {
         let parser = Parser::new();
         let parseres = parser.parse(&s);
         println!("{:#?}", parseres);
-        let mut xmireader = XmiReader::new();
-        xmireader.package = parseres.ok().expect("Could not parse");
-        xmireader
+        XmiReader::_new(parseres.ok().expect("Could not parse"))
     }
 
-    fn evaluate(&self, xpath: &str) -> Value {
-        let root = self.doc().root();
+    pub fn evaluate_root(&'a self, xpath: &str) -> Value<'d>{
+        self.evaluate(self.inner.doc().root(), xpath)
+    }
+
+    pub fn evaluate<N>(&'a self, node: N, xpath: &str) -> Value<'d>
+        where N: Into<Node<'d>> {
         let context = EvaluationContext::new(
-            root,
+            node,
             &self.functions,
             &self.variables,
             &self.namespaces,
@@ -53,19 +87,15 @@ impl<'a> XmiReader<'a> {
         xpath.evaluate(&context).ok().unwrap()
     }
 
-    fn doc(&self) -> Document {
-        self.package.as_document()
-    }
-
-    pub fn print(&mut self, file: &str) {
+    pub fn print(&'a self, file: &str) {
+        let doc = self.inner.doc();
         let mut f = File::create(file).expect("Could not create file");
-        let doc = self.doc();
-        format_document(&doc, &mut f).ok().expect("unable to output XML");
+        println!("{:?}", doc);
+        format_document(doc, &mut f).expect("Error formatting document");
     }
 
-    pub fn test(&self) {
-        let doc = self.doc();
-
+    pub fn test(&'a self) {
+        let doc = self.inner.doc();
         let hello = doc.create_element("hello");
         hello.set_attribute_value("planet", "Earth");
         let comment = doc.create_comment("What about other planets?");
